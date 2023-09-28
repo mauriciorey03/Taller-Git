a3ed94b40295f66326a3117a843c568c37a92926
mauriciorey03 <93847845+mauriciorey03@users.noreply.github.com>
2023-09-28 09:29:10 -0500

    Add files via upload

--- git3/todo.js
+++ git3/todo.js
@@ -1,35 +1,27 @@
-function addTask() {
-  // Crea una nueva tarea
-  var task = document.createElement("li");
-  task.classList.add("task");
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("contenedor-list")
 
-  // Crea un input de texto para la tarea
-  var input = document.createElement("input");
-  input.type = "text";
-  input.placeholder = "Agregar tarea";
-  task.appendChild(input);
-
-  // Crea una casilla de checkbox para la tarea
-  var checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.id = "checkbox";
-  task.appendChild(checkbox);
-
-  // Crea un botón para eliminar la tarea
-  var deleteButton = document.createElement("button");
-  deleteButton.textContent = "Eliminar";
-  deleteButton.id = "delete";
-  task.appendChild(deleteButton);
-
-  // Agrega la tarea a la lista
-  document.querySelector("#tasks").appendChild(task);
-
-  // Asigna un evento al botón para eliminar la tarea
-  deleteButton.addEventListener("click", function() {
-    // Elimina la tarea de la lista
-    task.remove();
-  });
+function addTarea(){
+    if (inputBox.value === '' ){
+        alert("Debes agregar una tarea")}
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
 }
 
-// Asigna un evento al input para agregar una tarea
-document.querySelector("#add").addEventListener("click", addTask);
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.ClassList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+
